8e54ee0cc167227bca39bf1e0d2cc8eadd064184
dshovchko <d.shovchko@gmail.com>
2017-11-13 16:20:21 +0200

    change composer.json and some style changes

--- src/Logger.php
+++ src/Logger.php
@@ -12,153 +12,153 @@
 namespace Debulog;
 
 abstract class Logger implements LoggerInterface {
-        
-        /**
-         *      @var string
-         */
-        protected $dir;
-    
-        /**
-         *      @var string
-         */
-        protected $prefix;
-        
-        /**
-         *      @var boolean
-         */
-        protected $ondebug;
-        
-        /**
-         *      @var array
-         */
-        protected $_messages = array();
-        
-        /**
-         *      @var array
-         */
-        protected $_errors = array();
-        
-        /**
-         *      @var array
-         */
-        protected $_debugs = array();
-        
-        /**
-         *      Logger constructor
-         *
-         */
-        public function __construct($dir, $prefix='my', $debug=false)
+
+    /**
+     *      @var string
+     */
+    protected $dir;
+
+    /**
+     *      @var string
+     */
+    protected $prefix;
+
+    /**
+     *      @var boolean
+     */
+    protected $ondebug;
+
+    /**
+     *      @var array
+     */
+    protected $_messages = array();
+
+    /**
+     *      @var array
+     */
+    protected $_errors = array();
+
+    /**
+     *      @var array
+     */
+    protected $_debugs = array();
+
+    /**
+     *      Logger constructor
+     *
+     */
+    public function __construct($dir, $prefix='my', $debug=false)
+    {
+        $this->dir = $dir;
+        $this->prefix = $prefix;
+        $this->ondebug = $debug;
+
+        if ($this->ondebug === TRUE)
         {
-                $this->dir = $dir;
-                $this->prefix = $prefix;
-                $this->ondebug = $debug;
-                
-                if ($this->ondebug === TRUE)
-                {
-                        $this->_debugs[] = PHP_EOL . 'start debugging at ' . strftime('%d.%m.%Y %H:%M:%S ') . PHP_EOL;
-                }
-                
-                register_shutdown_function(array($this, 'shutdown'));
-        }
-        
-        /**
-         *      Add message to logger buffer
-         *
-         *      @param string $message Message text
-         *
-         */
-        public function add($message)
-        {
-                $this->_messages[] = strftime('%d.%m.%Y %H:%M:%S ') . $message . PHP_EOL;
-                $this->debug($message);
+            $this->_debugs[] = PHP_EOL . 'start debugging at ' . strftime('%d.%m.%Y %H:%M:%S ') . PHP_EOL;
         }
 
-        /**
-         *      Add error message to logger buffer
-         *
-         *      @param string $message Message text
-         *
-         */
-        public function error($message)
+        register_shutdown_function(array($this, 'shutdown'));
+    }
+
+    /**
+     *      Add message to logger buffer
+     *
+     *      @param string $message Message text
+     *
+     */
+    public function add($message)
+    {
+        $this->_messages[] = strftime('%d.%m.%Y %H:%M:%S ') . $message . PHP_EOL;
+        $this->debug($message);
+    }
+
+    /**
+     *      Add error message to logger buffer
+     *
+     *      @param string $message Message text
+     *
+     */
+    public function error($message)
+    {
+        $this->_errors[] = strftime('%d.%m.%Y %H:%M:%S ') . $message . PHP_EOL;
+        $this->debug('ERROR: '.$message);
+    }
+
+    /**
+     *      Add debug message to logger buffer
+     *
+     *      @param string $message Message to log
+     *
+     */
+    public function debug($message)
+    {
+        if ($this->ondebug === TRUE)
         {
-                $this->_errors[] = strftime('%d.%m.%Y %H:%M:%S ') . $message . PHP_EOL;
-                $this->debug('ERROR: '.$message);
+            $this->_debugs[] = $message . PHP_EOL;
+        }
+    }
+
+    /**
+     *      Finish and sync all buffers to files
+     *
+     */
+    public function shutdown()
+    {
+        if ($this->ondebug === TRUE)
+        {
+            $this->_debugs[] = 'end of debugging at ' . strftime('%d.%m.%Y %H:%M:%S ') . PHP_EOL . PHP_EOL;
+        }
+        $this->sync();
+    }
+
+    /**
+     *      Sync all buffers to files
+     *
+     */
+    public function sync()
+    {
+        if (!empty($this->_messages))
+        {
+            $this->write($this->_messages, $this->dir . $this->prefix . '.log');
+            $this->_messages = array();
         }
 
-        /**
-         *      Add debug message to logger buffer
-         *
-         *      @param string $message Message to log
-         *
-         */
-        public function debug($message)
+        if (!empty($this->_debugs))
         {
-                if ($this->ondebug === TRUE)
-                {
-                        $this->_debugs[] = $message . PHP_EOL;
-                }
-        }
-        
-        /**
-         *      Finish and sync all buffers to files
-         *
-         */
-        public function shutdown()
-        {
-                if ($this->ondebug === TRUE)
-                {
-                        $this->_debugs[] = 'end of debugging at ' . strftime('%d.%m.%Y %H:%M:%S ') . PHP_EOL . PHP_EOL;
-                }
-                $this->sync();
+            $this->write($this->_debugs, $this->dir . $this->prefix . '_debug.log');
+            $this->debugs = array();
         }
 
-        /**
-         *      Sync all buffers to files
-         *
-         */
-        public function sync()
+        if (!empty($this->_errors))
         {
-                if (!empty($this->_messages))
-                {
-                        $this->write($this->_messages, $this->dir . $this->prefix . '.log');
-                        $this->_messages = array();
-                }
-                
-                if (!empty($this->_debugs))
-                {
-                        $this->write($this->_debugs, $this->dir . $this->prefix . '_debug.log');
-                        $this->debugs = array();
-                }
-                
-                if (!empty($this->_errors))
-                {
-                        $this->write($this->_errors, $this->dir . $this->prefix . '_error.log');
-                        $this->errors = array();
-                }
+            $this->write($this->_errors, $this->dir . $this->prefix . '_error.log');
+            $this->errors = array();
+        }
+    }
+
+    /**
+     *      Write text to file
+     *
+     *      @param string $messages
+     *      @param string $file
+     *
+     *      @throws Exception
+     *
+     */
+    protected function write($messages, $file)
+    {
+        $f = @fopen($file, 'a');
+        if ($f === false)
+        {
+            throw new \Exception("Logfile $file is not writeable!");
         }
 
-        /**
-         *      Write text to file
-         *
-         *      @param string $messages
-         *      @param string $file
-         *
-         *      @throws Exception
-         *
-         */
-        protected function write($messages, $file)
+        foreach($messages as $msg)
         {
-                $f = @fopen($file, 'a');
-                if ($f === false)
-                {
-                        throw new \Exception("Logfile $file is not writeable!");
-                }
-                
-                foreach($messages as $msg)
-                {
-                        fwrite($f, $msg);
-                }
-                
-                fclose($f);
+            fwrite($f, $msg);
         }
+
+        fclose($f);
+    }
 }
--- src/LoggerInterface.php
+++ src/LoggerInterface.php
@@ -12,35 +12,35 @@
 namespace Debulog;
 
 interface LoggerInterface {
-    
-        /**
-         *      Add message to logger buffer
-         *
-         *      @param string $message Message text
-         *
-         */
-        public function add($message);
 
-        /**
-         *      Add error message to logger buffer
-         *
-         *      @param string $message Message text
-         *
-         */
-        public function error($message);
+    /**
+     *      Add message to logger buffer
+     *
+     *      @param string $message Message text
+     *
+     */
+    public function add($message);
 
-        /**
-         *      Add debug message to logger buffer
-         *
-         *      @param string $message Message to log
-         *
-         */
-        public function debug($message);
+    /**
+     *      Add error message to logger buffer
+     *
+     *      @param string $message Message text
+     *
+     */
+    public function error($message);
 
-        /**
-         *      Sync all buffers to files
-         *
-         */
-        public function sync();
+    /**
+     *      Add debug message to logger buffer
+     *
+     *      @param string $message Message to log
+     *
+     */
+    public function debug($message);
+
+    /**
+     *      Sync all buffers to files
+     *
+     */
+    public function sync();
 
 }
